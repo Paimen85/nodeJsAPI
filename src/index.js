79c4c67666bf2503642a8f8ab4332bf21a714757
Paimen85 <evgeni.baranov@gmail.com>
2024-02-26 16:04:39 +0200

    Rsponse class created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import ip from "ip";
 import dotenv from "dotenv";
 import cors from "cors";
+import Response from "./domain/response.js";
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -15,7 +16,7 @@ app.use(
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send({ message: "Running" });
+  res.send(new Response(200, "OK", "Student API - v1.0", { students: {name: 'Evgeny'}}));
 });
 
 app.listen(PORT, () =>
